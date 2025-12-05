aa9d9e1a0308d772b114d42e733a07e10ddc4d18
Bivek Heka <bivek@example.com>
2025-12-06 00:14:00 +0545

    food and scoreboard added

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-from turtle import Screen,Turtle
+from turtle import Screen
 import time
 from snake import Snake
-screen=Screen()
+from food import Food
+from scoreboard import Scoreboard
 
+screen=Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
-
+food = Food()
+scoreboard=Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up,"Up")
@@ -22,8 +25,15 @@ game_is_on=True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+#detect collision with food
+    if snake.head.distance(food) < 15 :
+        food.refresh()
+        scoreboard.increase_score()
+
+
     
 screen.exitonclick()
 
+    
---
+++ scoreboard.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+ALIGN = "center"
+FONT = ("Lucida Console", 24 ,"normal")
+class Scoreboard(Turtle):
+
+    def __init__(self, shape = "classic", undobuffersize = 1000, visible = True):
+        super().__init__(shape, undobuffersize, visible)
+        self.score=0
+        self.color("white")
+        self.penup()
+        self.goto(0,270)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score} ", align=ALIGN , font=FONT)
+
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
