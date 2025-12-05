419995b71706f1a449afc91f0c912b85f851724d
Bivek Heka <bevillheka@gmail.com>
2025-12-06 01:21:10 +0545

    game over updated ccmpletly

--- snake.py
+++ snake.py
@@ -5,29 +5,36 @@ UP=90
 DOWN=270
 LEFT=180
 RIGHT=0
+colors = ["#00FF00", "#00CC00", "#009900", "#006600"]
 
 
 class Snake:
 
     def __init__(self):
-        self.segment=[]
+        self.segments=[]
         self.create_snake()
-        self.head=self.segment[0]
+        self.head=self.segments[0]
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment=Turtle(shape="square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segment.append(new_segment)
+            self.add_segment(position)
 
+    def add_segment(self,position):
+        new_segment=Turtle(shape="square")
+        color_index = len(self.segments) % len(colors)
+        new_segment.color(colors[color_index])
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
-        for seg_num in range(len(self.segment)-1, 0, -1):
-            x = self.segment[seg_num - 1].xcor()
-            y = self.segment[seg_num - 1].ycor()
-            self.segment[seg_num].goto(x, y)
+        for seg_num in range(len(self.segments)-1, 0, -1):
+            x = self.segments[seg_num - 1].xcor()
+            y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(x, y)
         
         self.head.forward(MOVE_DISTANCE)
 
@@ -49,3 +56,5 @@ class Snake:
     def right(self):
         if self.head.heading() != LEFT :
             self.head.setheading(RIGHT)
+
+    
