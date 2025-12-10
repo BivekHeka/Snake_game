a09695c2c360debedf4cbbff6b617e2d6f6463b0
Bivek Heka <bevillheka@gmail.com>
2025-12-10 18:04:08 +0545

    highscore added

--- scoreboard.py
+++ scoreboard.py
@@ -4,25 +4,28 @@ ALIGN = "center"
 FONT = ("Lucida Console", 24 ,"normal")
 class Scoreboard(Turtle):
 
-    def __init__(self, shape = "classic", undobuffersize = 1000, visible = True):
-        super().__init__(shape, undobuffersize, visible)
-        self.score=0
+    def __init__(self,):
+        super().__init__()
+        self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
-        self.goto(0,270)
+        self.goto(0,260)
         self.hideturtle()
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score} ", align=ALIGN , font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGN , font=FONT)
 
 
-    def game_over(self):
-        self.goto(0,0)
-        self.color("white")
-        self.write(f"GAME OVER !", align=ALIGN ,font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
