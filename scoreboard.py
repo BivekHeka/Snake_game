40e07b332f46a0d8fc86e1dc3bb19e6866d50558
Bivek Heka <bevillheka@gmail.com>
2025-12-10 22:15:05 +0545

    highscore updated with data.txt file

--- scoreboard.py
+++ scoreboard.py
@@ -7,11 +7,12 @@ class Scoreboard(Turtle):
     def __init__(self,):
         super().__init__()
         self.score = 0
-        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0,260)
         self.hideturtle()
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.update_scoreboard()
 
     def update_scoreboard(self):
@@ -22,6 +23,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt",mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
